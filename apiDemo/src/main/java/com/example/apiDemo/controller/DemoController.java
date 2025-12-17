e9aeda94a253f8b6fa2ccc7998d95ad143e2c895
Hector Velasco <hvelasco1804@gmail.com>
2025-12-17 17:45:44 -0600

    jenkins demo first commit

---
+++ apiDemo/src/main/java/com/example/apiDemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.apiDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apiJenkinsDemo/")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Respuesta de la API: OK";
+    }
+    public int calculateDiscount(int price) {
+        if (price < 0) throw new IllegalArgumentException("El precio no puede ser negativo");
+        return price - 10;
+    }
+}
