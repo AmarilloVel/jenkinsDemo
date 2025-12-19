0bd2933c40f107852797e482c7ba8502a10a53a4
Hector Velasco <hvelasco1804@gmail.com>
2025-12-19 04:26:26 -0600

    failed test

--- apiDemo/src/test/java/com/example/apiDemo/ApiDemoApplicationTests.java
+++ apiDemo/src/test/java/com/example/apiDemo/ApiDemoApplicationTests.java
@@ -26,6 +26,6 @@ class ApiDemoApplicationTests {
         DemoController controller = new DemoController();
         // PARA LA DEMO: Cambia el 90 por un 50 para que el test falle
         // assertEquals(50, controller.calcularDescuento(100));
-        assertEquals(90, controller.calculateDiscount(100));
+        assertEquals(50, controller.calculateDiscount(100));
     }
 }
