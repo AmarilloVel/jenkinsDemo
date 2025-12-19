a12d967b650b4ad7a848f575cc7049bfd269b876
Hector Velasco <hvelasco1804@gmail.com>
2025-12-18 21:29:59 -0600

    create tests docker and jenkins files

--- apiDemo/src/test/java/com/example/apiDemo/ApiDemoApplicationTests.java
+++ apiDemo/src/test/java/com/example/apiDemo/ApiDemoApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.apiDemo;
 
+import com.example.apiDemo.controller.DemoController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApiDemoApplicationTests {
@@ -10,4 +13,19 @@ class ApiDemoApplicationTests {
 	void contextLoads() {
 	}
 
+
+    @Test
+    void testDescuentoPositivo() {
+        DemoController controller = new DemoController();
+        // ESTO VA A PASAR (Verde)
+        assertEquals(90, controller.calculateDiscount(100));
+    }
+
+    @Test
+    void testDescuentoNegativo() {
+        DemoController controller = new DemoController();
+        // PARA LA DEMO: Cambia el 90 por un 50 para que el test falle
+        // assertEquals(50, controller.calcularDescuento(100));
+        assertEquals(90, controller.calculateDiscount(100));
+    }
 }
